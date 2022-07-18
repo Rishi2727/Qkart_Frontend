e2d3530d2180f88f40b17e117481a98f299a2af7
CrioUser <crio-user@criodo.com>
2022-07-18 14:39:27 +0530

    “ME_QKART_FRONTEND_V2_MODULE_REGISTER”

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,16 +1,89 @@
-import { Button, CircularProgress, Stack, TextField } from "@mui/material";
+import { Button, Stack, TextField } from "@mui/material";
 import { Box } from "@mui/system";
 import axios from "axios";
-import { useSnackbar } from "notistack";
 import React, { useState } from "react";
 import { config } from "../App";
 import Footer from "./Footer";
 import Header from "./Header";
 import "./Register.css";
 
-const Register = () => {
-  const { enqueueSnackbar } = useSnackbar();
 
+const Register = () => {
+  const [data, setData] = useState({ username: "", "password": "", "confirmPassword": "" });
+  const [message, setMessage] = useState("");
+  const api_url = config.endpoint;
+
+  const postRegister = () => {
+    var op = {}
+    axios(api_url + '/auth/register', { method: 'POST', data: (data) })
+      .then((res) => res)
+      .then(function (data){
+        op = data
+        console.log(op)
+        setMessage("Registration success .");
+        setTimeout(function () {
+          setData({ username: "", "password": "", "confirmPassword": "" });
+          document.getElementById("username").value = "";
+          document.getElementById("password").value = "";
+          document.getElementById("confirmPassword").value = "";
+          document.getElementById("btn").blur();
+          setMessage("");
+        },3000)
+      })
+      .catch(function (error) {
+        if (error.response && error.response.status && error.response.status===400) {
+          setMessage(" Username is already taken success ");
+        }
+        else if (error.response && error.response.status && error.response.status === 201) { 
+          setMessage("Registration success .");
+        }
+        else if (error.response && error.response.data && error.response.data.message) {
+          setMessage(error.response.data.message);
+        }
+        setMessage("Request success : Username is already taken OR Username is already exists");
+      });
+
+  }
+
+  const handleChange = (e) => {
+    const newData = {...data}
+    newData[e.target.name] = e.target.value
+    setData(newData)
+  }
+
+  const handleHcFormSubmit = (e) => {
+    if (data.username==="") {
+      setMessage("Username required");
+      return;
+    }
+    if (data.username.length < 6) {
+      setMessage("Username must be atleast 6 characters length");
+      return;
+    }
+    
+    if (data.password==="") {
+      setMessage("Password required");
+      return;
+    }
+    if (data.password.length < 6) {
+      setMessage("Password must be atleast 6 characters length");
+      return;
+    }
+    if (data.password !== data.confirmPassword) {
+      setMessage("Confirm Password do not match");
+      return;
+    }
+    // setMessage("");
+    postRegister()
+  }
+
+
+
+
+  // const { enqueueSnackbar } = useSnackbar();
+  // const { username } = useState();
+  // const { password } = useState();
+  // const { confirmPassword } = useState();
 
   // TODO: CRIO_TASK_MODULE_REGISTER - Implement the register function
   /**
@@ -35,8 +108,13 @@ const Register = () => {
    *      "message": "Username is already taken"
    * }
    */
-  const register = async (formData) => {
-  };
+
+  // const register = async (formData) => {
+  //   console.log(formData)
+    
+      
+  // };
+
 
   // TODO: CRIO_TASK_MODULE_REGISTER - Implement user input validation logic
   /**
@@ -58,7 +136,7 @@ const Register = () => {
    */
   const validateInput = (data) => {
   };
-
+  var message_className = message === "" ? 'alert alert-warning d-none' : 'alert alert-warning';
   return (
     <Box
       display="flex"
@@ -67,6 +145,7 @@ const Register = () => {
       minHeight="100vh"
     >
       <Header hasHiddenAuthButtons />
+      <Box className={message_className} role="alert" >{message}</Box>
       <Box className="content">
         <Stack spacing={2} className="form">
           <h2 className="title">Register</h2>
@@ -77,6 +156,7 @@ const Register = () => {
             title="Username"
             name="username"
             placeholder="Enter Username"
+            onChange={handleChange}
             fullWidth
           />
           <TextField
@@ -86,6 +166,7 @@ const Register = () => {
             name="password"
             type="password"
             helperText="Password must be atleast 6 characters length"
+            onChange={handleChange}
             fullWidth
             placeholder="Enter a password with minimum 6 characters"
           />
@@ -94,18 +175,14 @@ const Register = () => {
             variant="outlined"
             label="Confirm Password"
             name="confirmPassword"
+            onChange={handleChange}
             type="password"
             fullWidth
           />
-           <Button className="button" variant="contained">
+           <Button id="btn" className="button" variant="contained" onClick={handleHcFormSubmit}>
             Register Now
            </Button>
-          <p className="secondary-action">
-            Already have an account?{" "}
-             <a className="link" href="#">
-              Login here
-             </a>
-          </p>
+          <p className="secondary-action">Already have an account?{" "}<a className="link" href="#">Login here</a></p>
         </Stack>
       </Box>
       <Footer />
